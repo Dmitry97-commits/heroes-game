d3186be193b5fcadb976e6e0645f42808c0c5a5e
Dmitry <dmitrychichlo@gmail.com>
2021-03-13 20:10:35 +0300

    fix recovery

--- pers1.py
+++ pers1.py
@@ -9,6 +9,8 @@ class Dima(Character):
         self._armor = _armor
         self.counter_active = 1
         self.flag_active = False
+        max_health = self._health
+
 
     def __str__(self):
         return f"{self._name},{self._damage},{self._health},{self.counter_active}"
@@ -36,14 +38,19 @@ class Dima(Character):
 
 
     def recovery(self):# добавить макс значение стамины , на данный момент , поменять стамину на хэлс
-        stamina_rec = self._health + (Dima.health * 0.1)
-        if stamina_rec > Dima.health:
-            self._health = Dima.health
+        max_health = self._health
+        stamina_rec = self._health + (max_health * 0.1)
+        if stamina_rec > max_health:
+            self._health = max_health
+            if not self.flag_active :
+                self.counter_active -= 1
+                self.check_flag_active()
         else:
             self._health = stamina_rec
-        if not self.flag_active :
-            self.counter_active -= 1
-            self.check_flag_active()
+            if not self.flag_active :
+                self.counter_active -= 1
+                self.check_flag_active()
+
 
     def activate_ability(self,character): # блокировка
         self.__k_damage = 3
@@ -56,15 +63,22 @@ class Dima(Character):
         if self.counter_active == 0 :
             self.flag_active = True
 
-    @property
-    def name(self):
-        return self._name
-    @property
-    def damage(self):
-        return self._damage
-    @property
-    def stamina(self):
-        return self._health
-    @property
-    def active(self):
-        return self.counter_active
+@property
+def name(self):
+    return self._name
+@property
+def damage(self):
+    return self._damage
+@property
+def stamina(self):
+    return self._health
+@property
+def active(self):
+    return self.counter_active
+
+
+h1 = Dima(0,2,1,"da",300)
+print(h1.health)
+h1.taking_damage(100)
+print(h1.health)
+
